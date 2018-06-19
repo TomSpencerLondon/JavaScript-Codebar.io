ba860aecbd3f173765c985e2286e7cabff67948a
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-19 23:15:36 +0100

    Add lessons upto JQuery practice for To Do list with JavaScript and jQuery

---
+++ Javascript_Lesson_3_JQuery/script.js
@@ -0,0 +1,29 @@
+/* Exercise 1: Wish list */
+
+$(document).ready(function(){
+
+function addToList(item){
+  $('#items').append("<li>" + item + "<span class='label pending'>" + "Pending" + "</span></li>");
+}
+
+$(document).on('click', '#add-to-list', function() {
+  addToList($('#item').val());
+  focus();
+  updateTotal();
+});
+
+$(document).on('click', ".pending", function(){
+  var li_node = $(this).parent();
+  li_node.addClass('success').append("<span class='" + "label success'>Done!</span>");
+  li_node.addClass('completed');
+  $(this).remove();
+  updateTotal();
+});
+
+
+  function updateTotal(){
+    var pending = $('.pending').length;
+    var completed = $('.completed').length;
+    $('.total').text('Pending: ' + pending + ' Completed: ' + completed);
+  }
+});
