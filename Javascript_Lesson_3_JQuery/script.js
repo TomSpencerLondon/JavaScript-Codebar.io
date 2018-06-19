705cfb20a59dccbc59032a22d1defecbed55d5e1
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-19 23:19:49 +0100

    Add Build A Colour Picker Project

--- Javascript_Lesson_3_JQuery/script.js
+++ Javascript_Lesson_3_JQuery/script.js
@@ -6,6 +6,12 @@ function addToList(item){
   $('#items').append("<li>" + item + "<span class='label pending'>" + "Pending" + "</span></li>");
 }
 
+function updateTotal(){
+  var pending = $('.pending').length;
+  var completed = $('.completed').length;
+  $('.total').text('Pending: ' + pending + ' Completed: ' + completed);
+}
+
 $(document).on('click', '#add-to-list', function() {
   addToList($('#item').val());
   focus();
@@ -20,10 +26,4 @@ $(document).on('click', ".pending", function(){
   updateTotal();
 });
 
-
-  function updateTotal(){
-    var pending = $('.pending').length;
-    var completed = $('.completed').length;
-    $('.total').text('Pending: ' + pending + ' Completed: ' + completed);
-  }
 });
