ad10c79c29b5c764f68d7b4eedffa7bb05efc605
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-23 13:35:36 +0100

    Add jQuery function for adding color to color picker and preview

--- Javascript_Lesson_3_JQuery/Build_A_ColourPicker/script.js
+++ Javascript_Lesson_3_JQuery/Build_A_ColourPicker/script.js
@@ -1,1 +1,21 @@
 /* Exercise 2: Color picker */
+$(document).ready(function(){
+
+  function setPreviewColor(color){
+  $('.preview').css('background-color', color);
+  };
+
+  function addBox(color){
+    $('#colors').prepend('<div class="item" style="background-color: ' + color + ';"></div>');
+  }; 
+
+  $(document).on('click', "#add-to-favorite", function() {
+    console.log("This is the add to favorite button");
+    addBox($('#color').val());
+  });
+
+  $("#color").keyup(function() {
+  setPreviewColor($('#color').val());
+  $('.color-code').text($('.preview').css('background-color'));
+  });
+});
